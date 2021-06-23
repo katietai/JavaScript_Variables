dd7a09bd5d9cde5137d75cc005d41b4ecb9cf7cd
Taiyanna <simstaiyanna@yahoo.com>
2021-06-22 22:28:55 -0400

    new variables

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ console.log(
 );
 
 // Exercise 1
+ const firstName ="Taiyanna";
+ let lastName ='Sims';
+ var age= 29;
 
 // Exercise 2
-
+let fullName = firstName +""+lastName;
+let templatefullName = `${firstName} ${lastName}`;
+console.log("fullName:" , fullName);
+console.log("templatefullName" , fullName);
 // Exercise 3
+
+let city ="Atlanta";
+let pastime =`kickboxing, movies, and coding`;
+let myStory =`My name is ${fullName}.I live in ${city}.Things that I enjoy are ${pastime}.`;
+console.log(myStory);
